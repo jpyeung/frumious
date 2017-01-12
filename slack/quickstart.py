44f464b07844231afbc5ede9dd94cf3206b7b5bb
James Yeung <jamespyeung@alum.mit.edu>
2017-01-11 21:15:58 -0500

    Modifying script to run on sheet called Sync.

--- slack/quickstart.py
+++ slack/quickstart.py
@@ -179,11 +179,11 @@ def main():
             service = discovery.build('sheets', 'v4', http=http,
                                       discoveryServiceUrl=discoveryUrl)
             # Range of values we care about here
-            headerRange = 'Sheet1!A1:1'
+            headerRange = 'Sync!A1:1'
             header = service.spreadsheets().values().get(
                 spreadsheetId=spreadsheetId, range=headerRange).execute()
             columns = header.get('values', [])[0]
-            rangeName = 'Sheet1!A2:Z'
+            rangeName = 'Sync!A2:Z'
             result = service.spreadsheets().values().get(
                 spreadsheetId=spreadsheetId, range=rangeName).execute()
             values = result.get('values', [])
